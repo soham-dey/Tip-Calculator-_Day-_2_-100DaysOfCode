56fb2d49db50cf397150f18f5888981b45ede3cc
SOHAM DEY <deysoham.1999@gmail.com>
2020-12-08 07:20:27 +0000

    Made no. of people into variable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-total_bill= float(input("Enter total bill amount:"))
-tip_percent=float(input("Enter tip percentage:"))
-bill_ph_with_tip= (total_bill/5)*(1+round(tip_percent/100, 2))
+total_bill= float(input("Enter total bill amount: "))
+tip_percent=float(input("Enter tip percentage: "))
+people=int(input("How many people tosplit the bill into: "))
+bill_ph_with_tip= (total_bill/people)*(1+round(tip_percent/100, 2))
 rounded_bill_with_tip= round(bill_ph_with_tip, 2)
 print(f"The bill with tip for each person is: {rounded_bill_with_tip}")
 
