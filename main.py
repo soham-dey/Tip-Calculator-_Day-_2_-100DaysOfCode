db74e2c1868235be6b2f0d37a03408025681c901
SOHAM DEY <deysoham.1999@gmail.com>
2020-12-08 07:17:18 +0000

    Made tip as variable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 total_bill= float(input("Enter total bill amount:"))
-bill_ph_with_tip= (total_bill/5)*1.12
+tip_percent=float(input("Enter tip percentage:"))
+bill_ph_with_tip= (total_bill/5)*(1+round(tip_percent/100, 2))
 rounded_bill_with_tip= round(bill_ph_with_tip, 2)
 print(f"The bill with tip for each person is: {rounded_bill_with_tip}")
 
